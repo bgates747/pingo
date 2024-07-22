807603a2b962a7b4ef6cc5964dbd6780afaa44b1
bgates747 <brandon.gates@gmail.com>
2024-07-22 11:59:39 -0400

    initial linux_lookaround example copied from linux_window

---
+++ example/linux_lookaround/linux_lookaround_backend.c
@@ -0,0 +1,93 @@
+#include "linux_lookaround_backend.h"
+
+#include "render/renderer.h"
+#include "render/texture.h"
+#include "render/pixel.h"
+#include "render/depth.h"
+
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+#include <X11/Xos.h>
+
+Vec4i rect;
+Vec2i totalSize;
+
+PingoDepth * zetaBuffer;
+Pixel * frameBuffer;
+
+Display *dis = 0;
+int screen;
+Window win;
+GC gc;
+XImage * img = 0;
+Visual *visual;
+
+void init_x() {
+    if (dis != 0)
+        return;
+    dis=XOpenDisplay((char *)0);
+    screen=DefaultScreen(dis);
+    unsigned long black, white;
+    black = BlackPixel(dis, DefaultScreen(dis));
+    white = BlackPixel(dis, DefaultScreen(dis));
+    win=XCreateSimpleWindow(dis,DefaultRootWindow(dis),0,0, totalSize.x, totalSize.y, 5, white, black);
+    XSelectInput(dis, win, ExposureMask | KeyPressMask);
+    XSetStandardProperties(dis,win,"My Window","HI!",None,NULL,0,NULL);
+    XSelectInput(dis, win, ExposureMask|ButtonPressMask|KeyPressMask);
+    XMapWindow(dis, win);
+    gc=XCreateGC(dis, win, 0,0);
+    visual = DefaultVisual(dis, 0);
+};
+
+void init( Renderer * ren, BackEnd * backEnd, Vec4i _rect) {
+    rect = _rect;
+    init_x();
+}
+
+void beforeRender( Renderer * ren, BackEnd * backEnd) {
+    LinuxWindowBackEnd * this = (LinuxWindowBackEnd *) backEnd;
+
+}
+
+XImage *create_ximage(Display *display, Visual *visual, int width, int height)
+{
+    return XCreateImage(display, visual, 24, ZPixmap, 0, (char*)&frameBuffer[0], width, height, 32, 0);
+}
+
+void afterRender( Renderer * ren,  BackEnd * backEnd) {
+    if (!img)
+    img = create_ximage(dis, visual, totalSize.x, totalSize.y);
+
+    XEvent event;
+    XNextEvent(dis, &event);
+    //if (event.type == Expose)
+    {
+        XClearArea(dis, win, 0, 0, 1, 1, true);
+        XPutImage(dis, win, gc, img, 0,0,0,0,totalSize.x, totalSize.y );
+        //XSync(dis, 1);
+        XFlush(dis);
+    }
+
+}
+
+Pixel * getFrameBuffer( Renderer * ren,  BackEnd * backEnd) {
+    return frameBuffer;
+}
+
+PingoDepth * getZetaBuffer( Renderer * ren,  BackEnd * backEnd) {
+    return zetaBuffer;
+}
+
+void linuxWindowBackEndInit( LinuxWindowBackEnd * this, Vec2i size) {
+    totalSize = size;
+    this->backend.init = &init;
+    this->backend.beforeRender = &beforeRender;
+    this->backend.afterRender = &afterRender;
+    this->backend.getFrameBuffer = &getFrameBuffer;
+    this->backend.getZetaBuffer = &getZetaBuffer;
+    this->backend.drawPixel = 0;
+
+    zetaBuffer = malloc(size.x*size.y*sizeof (PingoDepth));
+    frameBuffer = malloc(size.x*size.y*sizeof (Pixel));
+    
+}
---
+++ example/linux_lookaround/main.c
@@ -0,0 +1,94 @@
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "render/material.h"
+#include "render/mesh.h"
+#include "render/object.h"
+#include "render/pixel.h"
+#include "render/renderer.h"
+#include "render/scene.h"
+
+#include "linux_lookaround_backend.h"
+
+#include "assets/viking.h"
+
+
+Pixel * loadTexture(char * filename, Vec2i size) {
+    Pixel * image = malloc(size.x*size.y*4);
+    FILE * file   = fopen(filename, "rb");
+    if (file == 0) {
+        printf("Error: Could not open file %s\n", filename);
+        exit(-1);
+    }
+    for (int i = 1023; i > 0; i--) {
+    for (int j = 0; j < 1024; j++) {
+            char r, g, b, a;
+            fread(&r, 1, 1, file);
+            fread(&g, 1, 1, file);
+            fread(&b, 1, 1, file);
+            fread(&a, 1, 1, file);
+            image[i*1024 + j] = pixelFromRGBA(r, g, b, a);
+        }
+    }
+    fclose(file);
+    return image;
+}
+
+int main(){
+    Vec2i size = {640, 480};
+
+    LinuxWindowBackEnd backend;
+    linuxWindowBackEndInit(&backend, size);
+
+    Renderer renderer;
+    rendererInit(&renderer, size,(BackEnd*) &backend );
+    rendererSetCamera(&renderer,(Vec4i){0,0,size.x,size.y});
+
+    Scene s;
+    sceneInit(&s);
+    rendererSetScene(&renderer, &s);
+
+    Object object;
+    object.mesh = &viking_mesh;
+
+    Pixel * image = loadTexture("assets/viking.rgba", (Vec2i){1024,1024});
+	Texture tex;
+	texture_init(&tex, (Vec2i){1024, 1024},image);
+	Material m;
+	m.texture = &tex;
+	object.material = &m;
+
+    sceneAddRenderable(&s, object_as_renderable(&object));
+
+    float phi = 0;
+    Mat4 t;
+
+	while (1) {
+        // PROJECTION MATRIX - Defines the type of projection used
+        renderer.camera_projection = mat4Perspective( 1, 2500.0,(float)size.x / (float)size.y, 0.6);
+
+        //VIEW MATRIX - Defines position and orientation of the "camera"
+        Mat4 v = mat4Translate((Vec3f) { 0,2,-35});
+
+        Mat4 rotateDown = mat4RotateX(-0.40); //Rotate around origin/orbit
+        renderer.camera_view = mat4MultiplyM(&rotateDown, &v );
+
+        //TEA TRANSFORM - Defines position and orientation of the object
+        object.transform = mat4RotateZ(3.142128);
+        t = mat4RotateZ(0);
+        object.transform = mat4MultiplyM(&object.transform, &t );
+
+        //SCENE
+        s.transform = mat4RotateY(phi);
+        phi += 0.01;
+
+        rendererRender(&renderer);
+        usleep(40000);
+	}
+
+    return 0;
+}
+
